5819d0cb46ff19989356a4aa639fe717f8589172
Eddie Quintero <eddyquyn2012@gmail.com>
2022-09-04 19:19:38 -0500

    interface parqueadero dao

---
+++ EasyParkingO18_14/src/main/java/com/O1814/EasyParkingO18_14/Dao/ParqueaderoDao.java
@@ -0,0 +1,9 @@
+package com.O1814.EasyParkingO18_14.Dao;
+
+import com.O1814.EasyParkingO18_14.Models.Parqueadero;
+import com.O1814.EasyParkingO18_14.Models.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParqueaderoDao extends CrudRepository<Parqueadero,String> {
+    
+}
